d6e276cad4203a421cd904f39b237bf008380c7c
Jihee You <jiheeyou@fe01>
2023-11-15 22:08:38 -0600

    slurm script without submitit

--- scripts/train_batch.sh
+++ scripts/train_batch.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-#SBATCH -J exatrkx_train
-#SBATCH -t 1440
-#SBATCH -p gpu_gce
-#SBATCH --gres=gpu:1
-#SBATCH -A fwk
-#SBATCH -q regular
-#SBATCH --cpus-per-task=12
-#SBATCH --signal=SIGUSR1@90
 
-srun python scripts/train.py $@
+#SBATCH --job-name=fermi2
+#SBATCH --output=dsi_log/fermi2.out
+#SBATCH --error=dsi_log/fermi2.err
+#SBATCH --time=12:00:00
+#SBATCH --partition=general
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem-per-cpu=16G
+#SBATCH --mail-type=ALL  # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=jiheeyou@rcc.uchicago.edu  # mail notification for the job
+
+srun python train.py --data-path /net/projects/fermi-2/CHEP2023.gnn.h5 --logdir /home/jiheeyou/NuGraph/scripts/dsi_log --name fermi2 --version semantic-filter --semantic --filter
