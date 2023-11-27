a800372fcc1bb7d0e77eeef75c7368b421e6e480
Shan Gao <shangaocog@gmail.com>
2023-11-27 11:58:03 -0600

    save log to user dir; add instr

--- scripts/train_batch_dsi.sh
+++ scripts/train_batch_dsi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=fermi2
-#SBATCH --output=/net/projects/fermi-2/logs/%A_%a.out
-#SBATCH --error=/net/projects/fermi-2/logs/%A_%a.err
+#SBATCH --output=/net/projects/fermi-2/logs/%u/%A_%a.out
+#SBATCH --error=/net/projects/fermi-2/logs/%u/%A_%a.err
 #SBATCH --time=12:00:00
 #SBATCH --partition=general
 #SBATCH --nodes=1
@@ -13,17 +13,21 @@
 
 
 
-# call this script in command line with:
-# sbatch scripts/train_batch_dsi.sh
+# Instructions:
+# - start from the login node (fe01)
+# - activate environment
+# - cd to ~/NuGraph
+# - modify params in train_batch_dsi.sh as needed
+# - in command line, run: sbatch scripts/train_batch_dsi.sh
 
-# or to run interactively, request session with:
+# Or to run interactively, request session with:
 # srun -p general -t "12:00:00" --mem "64G" --cpus-per-task 2 --gres gpu:1 --constraint a100 --pty /bin/bash
 
 
 
 ################ CHANGE ################
 # mlp features
-vtx_mlp_features=128
+vtx_mlp_features=256
 
 # aggregator
 vtx_aggr="lstm"
@@ -35,7 +39,7 @@ vtx_lstm_features=8
 epochs=80 
 
 # don't forget to also update the arguments of the python script call below: it should contain with (--logdir & --name) OR (--resume)
-# ckpt="epoch=57-step=272194.ckpt"
+# ckpt="epoch=58-step=276887.ckpt"
 #########################################
 
 
@@ -62,8 +66,8 @@ srun python scripts/train.py \
                  --vertex-mlp-feats ${vtx_mlp_features} \
                  --epochs ${epochs} \
                  --logdir ${logdir} \
-                 --name  "Vertex_Decoder_Search"\
-                #  --resume "${logdir}/Vertex_Decoder_Search/${log_name}/checkpoints/${ckpt}" \
+                 --name  "Vertex_Decoder_Search"            
+                #  --resume "${logdir}/Vertex_Decoder_Search/${log_name}/checkpoints/${ckpt}"
                 #  --limit_train_batches ${lim_train_batches}\
                 #  --limit_val_batches ${lim_val_batches}\
                 #  --num_nodes 4 \
